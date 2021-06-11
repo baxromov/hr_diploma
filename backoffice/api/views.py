49b0274a7514e7eb808b3ea80ffd74e93da6c381
Afzal Yusupov <afzal-yusupov@mail.ru>
2021-06-11 15:33:30 +0500

    add jwt to views.py

--- backoffice/api/views.py
+++ backoffice/api/views.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets
 from rest_framework import generics
-from rest_framework.response import Response
-
+from rest_framework_simplejwt import views as rest_framework_simplejwt_views
 from app import models
 from . import serializers
 from rest_framework import permissions
