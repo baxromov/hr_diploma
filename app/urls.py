071f6ad1fc7776455ab417c6bcec87add691a018
baxromov1 <baxromov.shahzodbek@gmail.com>
2021-06-11 15:08:55 +0500

    Salom 08.06.21

--- app/urls.py
+++ app/urls.py
@@ -2,14 +2,6 @@ from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
 from app.project_bot import BotAPIView
-from . import views
-
-# urlpatterns = [
-#     path('', views.index, name='index'),
-#     path('departmets/', views.departments, name='departmets'),
-#     path('question/<int:pk>', views.question, name='question'),
-#     path('ajax_departmets/', views.ajax_dep, name='ajax_department'),
-# ]
 
 # bot url
 
