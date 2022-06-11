cb95fe3a79ffa0ed594292201a3026e02a582529
baxromov <baxromov.shahzodbek@gmail.com>
2022-06-11 11:38:04 +0500

    add

--- project/settings.py
+++ project/settings.py
@@ -118,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'uz'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'Asia/Tashkent'
 
